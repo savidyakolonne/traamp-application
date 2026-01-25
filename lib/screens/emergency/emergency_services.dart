97fa440238019a0f67b7dc959a531a9b9c1242de
Dihara Sathmi <diharasathmi2004@gmail.com>
2026-01-26 00:02:27 +0530

    Connect Emergency Contacts screen from dashboard

---
+++ lib/screens/emergency/emergency_services.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EmergencyServices extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Emergency Services'),
+        ),
+        body: Center(
+          child: Text('Emergency Services Screen'),
+        ),
+      ),
+    );
+  }
+}
