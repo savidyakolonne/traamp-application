09836d35ad7a683257cc23200eee591910a82838
Diyana1111 <diyanasenadi1111@gmail.com>
2026-02-09 20:01:38 +0530

    add Firebase and utility imports

--- lib/screens/tourist/tourist_edit_profile.dart
+++ lib/screens/tourist/tourist_edit_profile.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import '../../list-data.dart';
+import '../../services/tourist.dart';
 
 class EditTouristProfile extends StatefulWidget {
   const EditTouristProfile({super.key});
