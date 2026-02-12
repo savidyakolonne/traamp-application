1e37db4dc54a8a641cb26b2da06cd0095cc6c76d
Diyana1111 <diyanasenadi1111@gmail.com>
2026-02-12 09:29:19 +0530

    add Firebase and utility imports in the edit guide profile

--- lib/screens/guide/guide_edit_profile.dart
+++ lib/screens/guide/guide_edit_profile.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import '../../list-data.dart';
 
 class EditGuideProfile extends StatefulWidget {
   const EditGuideProfile({super.key});
