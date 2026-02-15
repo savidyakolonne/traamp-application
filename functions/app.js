a673afc3f89f65ad8a17c718edd6a2faa9abd8ac
pramodya <pramodyalakmuthu443@gmail.com>
2026-02-15 12:30:36 +0530

    changes added app.js

--- functions/app.js
+++ functions/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import placesRouter from "./routes/places.routes.js";
 import userRouter from "./routes/users.mjs";
+import placeRouter from "./routes/place.routes.js";
 import activityRouter from "./routes/activity.routes.js";
 import packageRouter from "./routes/guide_package.mjs";
 
@@ -15,7 +16,7 @@ app.get("/", (_, res) => res.send("Server is running..."));
 
 app.use("/api/places", placesRouter);
 app.use("/api/users", userRouter);
-
+app.use("/api/place", placeRouter);
 app.use("/api/activity", activityRouter);
 app.use("/api/guidePackage", packageRouter);
 
