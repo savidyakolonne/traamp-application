72e53e5706fe2926923ed426572fa67115a89172
Limasha-Githmal <limashagithmal@gmail.com>
2026-02-25 10:47:20 +0530

    errors fixed

--- lib/screens/places/place_detail_screen.dart
+++ lib/screens/places/place_detail_screen.dart
@@ -5,35 +5,8 @@ import '../../screens/map/map_screen.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PlaceDetailScreen extends StatefulWidget {
-class PlaceDetailScreen extends StatefulWidget {
   final String placeId;
   const PlaceDetailScreen({required this.placeId});
-
-  @override
-  State<PlaceDetailScreen> createState() => _PlaceDetailScreenState();
-}
-
-class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
-  final PageController _pageController = PageController();
-  int _currentIndex = 0;
-
-  void _nextImage(int length) {
-    if (_currentIndex < length - 1) {
-      _pageController.nextPage(
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    }
-  }
-
-  void _previousImage() {
-    if (_currentIndex > 0) {
-      _pageController.previousPage(
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    }
-  }
 
   @override
   State<PlaceDetailScreen> createState() => _PlaceDetailScreenState();
@@ -65,23 +38,21 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffF4F5F7),
-      backgroundColor: const Color(0xffF4F5F7),
       body: FutureBuilder(
-        future: PlacesService.getPlaceById(widget.placeId),
         future: PlacesService.getPlaceById(widget.placeId),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
             return const Center(child: CircularProgressIndicator());
           }
 
           final Place place = snapshot.data!;
 
+          final double lat = (place.location['lat'] as num).toDouble();
+          final double lng = (place.location['lng'] as num).toDouble();
+
           return ListView(
             padding: EdgeInsets.zero,
-            padding: EdgeInsets.zero,
             children: [
-              // ================= IMAGE SLIDER =================
               // ================= IMAGE SLIDER =================
               SizedBox(
                 height: 280,
@@ -173,96 +144,6 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
               ),
 
               const SizedBox(height: 18),
-                height: 280,
-                child: Stack(
-                  children: [
-                    PageView.builder(
-                      controller: _pageController,
-                      itemCount: place.images.length,
-                      onPageChanged: (index) {
-                        setState(() => _currentIndex = index);
-                      },
-                      itemBuilder: (context, index) {
-                        return Image.network(
-                          place.images[index],
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                        );
-                      },
-                    ),
-
-                    // Back Button
-                    Positioned(
-                      top: 40,
-                      left: 16,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.black45,
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.arrow_back,
-                            color: Colors.white,
-                          ),
-                          onPressed: () => Navigator.pop(context),
-                        ),
-                      ),
-                    ),
-
-                    // Left Arrow
-                    Positioned(
-                      left: 10,
-                      top: 120,
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white,
-                        ),
-                        onPressed: _previousImage,
-                      ),
-                    ),
-
-                    // Right Arrow
-                    Positioned(
-                      right: 10,
-                      top: 120,
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.white,
-                        ),
-                        onPressed: () => _nextImage(place.images.length),
-                      ),
-                    ),
-
-                    // Indicator Dots
-                    Positioned(
-                      bottom: 15,
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(
-                          place.images.length,
-                          (index) => AnimatedContainer(
-                            duration: const Duration(milliseconds: 300),
-                            margin: const EdgeInsets.symmetric(horizontal: 4),
-                            height: 8,
-                            width: _currentIndex == index ? 18 : 8,
-                            decoration: BoxDecoration(
-                              color: _currentIndex == index
-                                  ? Colors.white
-                                  : Colors.white54,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 18),
-
 
               // ================= TITLE =================
               Padding(
@@ -276,7 +157,6 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
                 ),
               ),
 
-
               const SizedBox(height: 6),
 
               // ================= LOCATION =================
@@ -289,9 +169,7 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
                       size: 18,
                       color: Color(0xff1BA672),
                     ),
-
                     const SizedBox(width: 4),
-                    
                     Text(
                       "${place.district}, ${place.province}",
                       style: const TextStyle(fontSize: 14, color: Colors.grey),
@@ -301,7 +179,8 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
               ),
 
               const SizedBox(height: 14),
-              //map navigate button ------------------------------------------------------------------------
+
+              // ================= MAP BUTTON =================
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: SizedBox(
@@ -318,8 +197,6 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
                       ),
                     ),
                     onPressed: () {
-                      final lat = (place.location['lat'] as num).toDouble();
-
                       Navigator.push(
                         context,
                         MaterialPageRoute(
@@ -333,7 +210,7 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
                   ),
                 ),
               ),
-              //------------------------------------------------------------------------------------------
+
               const SizedBox(height: 22),
 
               // ================= QUICK INFO =================
@@ -353,7 +230,6 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
 
                       const SizedBox(height: 14),
 
-                      // VISITING HOURS (FULL ROW)
                       if (place.visitingHours != null)
                         _fullWidthInfoCard(
                           icon: Icons.access_time,
@@ -368,7 +244,6 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
 
                       const SizedBox(height: 14),
 
-                      // BEST TIME (FULL ROW)
                       if (place.bestTimeToVisit != null)
                         _fullWidthInfoCard(
                           icon: Icons.wb_sunny,
@@ -465,82 +340,6 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
             ],
           );
         },
-      ),
-    );
-  }
-
-  // ================= FULL WIDTH CARD =================
-  Widget _fullWidthInfoCard({
-    required IconData icon,
-    required String title,
-    required String line1,
-    required String line2,
-    required Color iconColor,
-    required Color iconBg,
-    required Color cardColor,
-  }) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: cardColor,
-        borderRadius: BorderRadius.circular(18),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.08),
-            blurRadius: 8,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: iconBg,
-              borderRadius: BorderRadius.circular(14),
-            ),
-            child: Icon(icon, color: iconColor, size: 22),
-          ),
-
-          const SizedBox(width: 14),
-
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(fontSize: 14, color: Colors.grey),
-                ),
-
-                const SizedBox(height: 6),
-
-                Text(
-                  line1,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-
-                if (line2.isNotEmpty)
-                  Padding(
-                    padding: const EdgeInsets.only(top: 4),
-                    child: Text(
-                      line2,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.black54,
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
