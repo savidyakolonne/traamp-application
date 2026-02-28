9898cca4e89d0cba7b47f6d28179a0746f1d3599
Diyana1111 <diyanasenadi1111@gmail.com>
2026-02-28 22:28:18 +0530

    Resolve stash conflict and keep local.properties deleted

--- lib/screens/guide/guide_edit_profile.dart
+++ lib/screens/guide/guide_edit_profile.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import '../../list-data.dart';
 
 class EditGuideProfile extends StatefulWidget {
   const EditGuideProfile({super.key});
