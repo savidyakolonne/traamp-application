9561f32abbc592e875034051f5f15c8958b98f28
Dihara2004 <diharasathmi2004@gmail.com>
2026-03-04 20:51:46 +0530

    Create backend part in setting screen dart file

--- lib/components/settings_screen.dart
+++ lib/components/settings_screen.dart
@@ -47,80 +47,12 @@ class _SettingsState extends State<Settings> {
       loggedOut = false;
     }
   }
-
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(automaticallyImplyLeading: false, title: Text("Settings")),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.account_circle_outlined, size: 30),
-            title: Text("Profile"),
-            subtitle: Text("View your profile"),
-            trailing: Icon(Icons.menu),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.mode_night_outlined, size: 30),
-            title: Text("Theme"),
-            subtitle: Text("Change your theme"),
-            trailing: Icon(Icons.menu),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.lock_outline_rounded, size: 30),
-            title: Text("Privacy"),
-            subtitle: Text("Secure your account with privacy"),
-            trailing: Icon(Icons.menu),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.volunteer_activism_outlined, size: 30),
-            title: Text("Help and Support"),
-            subtitle: Text("Get help from our team"),
-            trailing: Icon(Icons.menu),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.error_outline_rounded, size: 30),
-            title: Text("About"),
-            subtitle: Text("Version info, terms & conditions, privacy policy"),
-            trailing: Icon(Icons.menu),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.logout, size: 30),
-            title: Text("Logout"),
-            onTap: () async {
-              await signOutUser();
-              if (loggedOut) {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
-                );
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Logged out successfully."),
-                    backgroundColor: Colors.green,
-                  ),
-                );
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Error while logging out"),
-                    backgroundColor: Colors.red,
-                  ),
-                );
-              }
-            },
-          ),
-        ],
-      ),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
+
+
