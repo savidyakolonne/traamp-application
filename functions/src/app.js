c76478e54e289c9d882f5178d75c9ba62f1efb85
savidyakolonne <savidya.kolonne@gmail.com>
2026-03-06 23:20:03 +0530

    Reorganize backend to src/ folder, add controllers and routes

--- functions/src/app.js
+++ functions/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
-import placesRouter from "./routes/places.routes.js";
-import activityRouter from "./routes/activity.routes.js";
-import userRouter from "./routes/users.mjs";
+
+import guideRoutes from "./routes/guideRoutes.js"
+import activityRoutes from "./routes/activityRoutes.js" ;
+import placeRoutes from "./routes/placeRoutes.js" ;
 import packageRouter from "./routes/guide_package.mjs";
 import galleryRouter from "./routes/gallery.mjs";
-import guidesRouter from "./routes/guides.routes.js" ; 
+import userRoutes from "./routes/userRoutes.js";
+
 
 const app = express();
 
@@ -15,13 +17,13 @@ app.use(express.json());
 
 app.get("/", (_, res) => res.send("Server is running..."));
 
-app.use("/api/places", placesRouter);
-app.use("/api/users", userRouter);
-app.use("/api/activity", activityRouter);
+app.use("/api/places", placeRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/activity", activityRoutes);
 app.use("/api/guidePackage", packageRouter);
 app.use("/api/gallery", galleryRouter);
 
 //guides
-app.use("/api/guides", guidesRouter)
+app.use("/api/guides", guideRoutes) ;
 
 export default app;
