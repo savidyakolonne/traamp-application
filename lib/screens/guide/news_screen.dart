599e5476b1e622c4dc92be63ef1e61b89d71edd6
Limasha-Githmal <limashagithmal@gmail.com>
2026-03-09 19:40:27 +0530

    news frontend done

--- lib/screens/guide/news_screen.dart
+++ lib/screens/guide/news_screen.dart
@@ -16,7 +16,7 @@ class _NewsScreenState extends State<NewsScreen> {
   @override
   void initState() {
     super.initState();
-    newsFuture = NewsService.fetchNews();
+    newsFuture = NewsService.getNews();
   }
 
   @override
@@ -28,14 +28,22 @@ class _NewsScreenState extends State<NewsScreen> {
         future: newsFuture,
 
         builder: (context, snapshot) {
+          /// Loading
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
 
+          /// Error
           if (snapshot.hasError) {
             return const Center(child: Text("Error loading news"));
           }
 
+          /// No Data
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text("No news available"));
+          }
+
+          /// Data Loaded
           final newsList = snapshot.data!;
 
           return ListView.builder(
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,21 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../app_config.dart';
 import '../models/news_model.dart';
 
 class NewsService {
-  // replace with your backend URL
-  static const String baseUrl =
-      "https://yourproject.cloudfunctions.net/api/news";
+  static const String baseUrl = "${AppConfig.SERVER_URL}/api/news";
 
-  static Future<List<News>> fetchNews() async {
-    final response = await http.get(Uri.parse(baseUrl));
+  static Future<List<News>> getNews() async {
+    final res = await http.get(Uri.parse(baseUrl));
 
-    if (response.statusCode == 200) {
-      final List data = json.decode(response.body);
+    final List data = jsonDecode(res.body);
 
-      return data.map((json) => News.fromJson(json)).toList();
-    } else {
-      throw Exception("Failed to load news");
-    }
+    return data.map((e) => News.fromJson(e)).toList();
   }
 }
