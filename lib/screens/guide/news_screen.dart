aa2d35c88f5adb1a9a12e41e013dc021c764676a
Limasha-Githmal <limashagithmal@gmail.com>
2026-03-09 19:28:43 +0530

    news_screen.dart file update

---
+++ lib/screens/guide/news_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../../services/news_service.dart';
+import '../../models/news_model.dart';
+import '../../widgets/news_card.dart';
+
+class NewsScreen extends StatefulWidget {
+  const NewsScreen({super.key});
+
+  @override
+  State<NewsScreen> createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  late Future<List<News>> newsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    newsFuture = NewsService.fetchNews();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Tourism News"), centerTitle: true),
+
+      body: FutureBuilder<List<News>>(
+        future: newsFuture,
+
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return const Center(child: Text("Error loading news"));
+          }
+
+          final newsList = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: newsList.length,
+
+            itemBuilder: (context, index) {
+              return NewsCard(news: newsList[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
