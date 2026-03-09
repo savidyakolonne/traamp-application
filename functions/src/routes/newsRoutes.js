7954c073491f9d64f68c0033a159d18bec120e66
Limasha-Githmal <limashagithmal@gmail.com>
2026-03-09 19:10:29 +0530

    newsRoutes.js file update

---
+++ functions/src/routes/newsRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const newsController = require("../controllers/newsController");
+
+router.get("/", newsController.getAllNews);
+
+router.get("/:id", newsController.getNewsById);
+
+router.post("/", newsController.createNews);
+
+router.delete("/:id", newsController.deleteNews);
+
+module.exports = router;
