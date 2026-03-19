bd91a4c7f4364b4bca60bbe9e51890d959957b63
savidyakolonne <savidya.kolonne@gmail.com>
2026-03-19 14:22:50 +0530

    feat(auth): add email verification and force verify endpoint

--- functions/src/routes/userRoutes.js
+++ functions/src/routes/userRoutes.js
@@ -19,8 +19,10 @@ const userRouter = Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 userRouter.post("/register-tourist", registerTourist);
-
 userRouter.post("/register-guide", upload.single("certificate"), registerGuide);
+
+userRouter.post("/loginWithEmail", loginWithEmail);
+userRouter.post("/get-user-data", getUserData);
 
 userRouter.put(
   "/update-guide-certificate",
@@ -28,18 +30,11 @@ userRouter.put(
   updateGuideCertificate,
 );
 
-userRouter.post("/loginWithEmail", loginWithEmail);
-
-userRouter.post("/get-user-data", getUserData);
-
 userRouter.put("/update-tourist-profile", updateTouristProfile);
-
 userRouter.put("/update-guide-profile", updateGuideProfile);
-
 userRouter.put("/update-guide-availability", updateGuideAvailability);
 
 userRouter.post("/user-logout", logoutUser);
-
 userRouter.delete("/delete-user", deleteUser);
 
 export default userRouter;
