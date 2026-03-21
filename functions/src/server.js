5b3c47e93caf7811ace2ee448b955b7c47fbcb14
savidyakolonne <savidya.kolonne@gmail.com>
2026-03-21 14:16:23 +0530

    refactor backend startup and firebase admin config

---
+++ functions/src/server.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
+});
