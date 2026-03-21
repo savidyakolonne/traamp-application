3ffa91b16912d01851acc826da5fbfcf3a9292ca
pramodya <pramodyalakmuthu443@gmail.com>
2026-03-21 16:45:43 +0530

    backend hosted

--- traamp-mobile/lib/app_config.dart
+++ traamp-mobile/lib/app_config.dart
@@ -1,9 +1,11 @@
 // Change Server URL globally
 class AppConfig {
-  static const SERVER_URL = 'http://localhost:5000';
+  static const SERVER_URL = 'https://traamp-application.onrender.com';
 
   // for the emulators => device ip
   // 'http://10.0.2.2:3000'
   // for using the web to preview
   // http://localhost:3000
+  // hosted
+  // https://traamp-application.onrender.com
 }
