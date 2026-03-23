e86903eb4bd64a7ac67b6bd7719e4d84f9f4b82c
pramodya <pramodyalakmuthu443@gmail.com>
2026-03-23 06:51:24 +0530

    application id changed

--- traamp-mobile/android/app/build.gradle.kts
+++ traamp-mobile/android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.traamp_frontend"
+        applicationId = "com.traam.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
